607c313f69c8c40bd33adf850ca23b4c18849428
allenchou <allenchou7@gmail.com>
2018-12-07 21:20:39 -0500

    add linear regression

--- app/static/analytics_app/app.components.js
+++ app/static/analytics_app/app.components.js
@@ -28,72 +28,23 @@
                 })
         };
 
+        ctrl.options = {};
+        ctrl.series = ['X', 'Y'];
+    }
+
+    function regressionCtrl($log, dataService) {
+        var ctrl = this;
+
         dataService.getLinearRegressions(["Log_GDP_per_capita", "Social_support"])
             .then(function (resp) {
                 $log.debug(resp);
+                ctrl.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+                ctrl.series = ['Series A', 'Series B'];
+
+                ctrl.data = [
+                    [65, 59, 80, 81, 56, 55, 40],
+                    [28, 48, 40, 19, 86, 27, 90]
+                ];
             });
-        /*
-        ctrl.data = [[{
-            x: -2,
-            y: -5
-        }, {
-            x: 0,
-            y: 3
-        }, {
-            x: 1,
-            y: 2
-        },
-            {
-                x: 4,
-                y: 5
-            }]];
-        */
-
-        ctrl.options = {};
-        ctrl.series = ['X', 'Y'];
-
-        ctrl.myChartObject = {};
-
-        ctrl.myChartObject.type = "ScatterChart";
-
-        ctrl.myChartObject.data = {
-            "cols": [
-                {id: "x", label: "x", type: "number"},
-                {id: "y", label: "y", type: "number"}
-            ], "rows": [
-                {
-                    c: [
-                        {v: "4"},
-                        {v: 3},
-                    ]
-                },
-                {
-                    c: [
-                        {v: "5"},
-                        {v: 31}
-                    ]
-                },
-                {
-                    c: [
-                        {v: "4"},
-                        {v: 1},
-                    ]
-                },
-                {
-                    c: [
-                        {v: "5"},
-                        {v: 2},
-                    ]
-                }
-            ]
-        };
-
-        ctrl.myChartObject.options = {
-            'title': 'How Much Pizza I Ate Last Night'
-        };
-    }
-
-    function regressionCtrl($log) {
-
     }
 }());
